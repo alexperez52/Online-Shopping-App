dac93aa5aff8541286479287612e68be37b21c45
Nikolas Kinalis <kinan69@mail.sunysuffolk.edu>
2020-02-09 12:08:42 -0500

    Removed test project, added maven project for application.

---
+++ ShoppingApplication/src/main/java/shopping/app/App.java
@@ -0,0 +1,13 @@
+package shopping.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
