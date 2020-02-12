61dcef27a789de94badf1a028253c495826e009a
Nikolas Kinalis <kinan69@mail.sunysuffolk.edu>
2020-02-12 10:36:50 -0500

    Laid out the packages for the project and wrote up the model. Also changed the Class diagram to reflect slight changes in some class fields/methods

--- ShoppingApplication/src/main/java/shopping/app/App.java
+++ ShoppingApplication/src/main/java/shopping/app/App.java
@@ -1,13 +1,16 @@
 package shopping.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
